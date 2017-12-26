acde0a0a381be0bc9550f45a622d7a70108c0c18
WangJu <wonder_land@foxmail.com>
2017-12-26 12:44:34 +0800

    init project

---
+++ app/src/main/java/tinyhope/github/com/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package tinyhope.github.com.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import tinyhope.github.com.topeka.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
